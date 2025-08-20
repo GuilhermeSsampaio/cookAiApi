fe5cda1dd6a11204d5d0e0edc74420d2eac92bf1
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-08-20 17:15:05 -0400

    tentando sem langchain

--- services/scrap.py
+++ services/scrap.py
@@ -1,35 +1,38 @@
-
-from langchain_google_genai import GoogleGenerativeAI
 from dotenv import load_dotenv
 from docling.document_converter import DocumentConverter
+from google import genai
+import os
 
 load_dotenv()
 
 converter = DocumentConverter()
-llm = GoogleGenerativeAI(model="gemini-1.5-flash")
+
+# Configure a API do Gemini
+client = genai.Client(api_key=os.getenv("GEMINI_API_KEY"))
 
 def scrap_recipe(url):
     """
     Scrape a recipe from the given URL and return its content.
     """
     try:
-        result =converter.convert(url)
+        result = converter.convert(url)
         docling_txt = result.document.export_to_markdown()
         
         print("Receita extraída com sucesso.")
-        prompt = """
+        prompt = f"""
         Resuma essa receita, passando os ingredientes, tempo de forno e o modo de preparo:
 
-        {text}
-        Caso não tenha o tempo de forno indique o recomendado
-        Traduza para o português
+        {docling_txt}
+        Caso não tenha o tempo de forno indique o recomendado.
+        Traduza para o português.
         """
-        result = llm.invoke(prompt.format(text=docling_txt)).strip()
-    
-        return result
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=prompt,
+        )
+        return response.text
     except Exception as error:
         return {"error": f"Failed to scrape recipe from {url}: {error}"} 
-
 
 # result = scrap_recipe("https://www.receitasnestle.com.br/receitas/bolo-de-cenoura-com-cobertura-de-brigadeiro")
 # print(result)
