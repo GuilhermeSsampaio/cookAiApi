990693939ed268d486892f1b6433223bfc528d42
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-08-20 20:13:28 -0400

    corrigindo main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
 from services.scrap import scrap_recipe
  
 app = FastAPI()
@@ -22,3 +21,10 @@ def extract_scrap_recipe(url: str):
     scrap_result = scrap_recipe(url)
     return scrap_result
 
+import os
+
+port = int(os.environ.get("PORT", 8000))
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
