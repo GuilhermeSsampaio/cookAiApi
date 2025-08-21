7e358f707b09ec5586e7898e79aae3212430ab29
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-08-20 20:50:06 -0400

    corrigindo o cors ou endpoints

--- main.py
+++ main.py
@@ -1,34 +1,34 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from services.scrap import scrap_recipe
- 
+
 app = FastAPI()
-
-import os
-
-port = int(os.environ.get("PORT", 8000))
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Adjust this to restrict origins in production
+    allow_origins=["*"],  # Ajuste para restringir em produção
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/")
+@app.get("/", include_in_schema=False)
+@app.head("/", include_in_schema=False)
 def read_root():
     return {"message": "Welcome to the CookAi API"}
 
-@app.post("/scrap")
-def extract_scrap_recipe(url: str):
+@app.get("/scrap")
+def extract_scrap_recipe_get(url: str = Query(...)):
     scrap_result = scrap_recipe(url)
     return scrap_result
 
-import os
-
-port = int(os.environ.get("PORT", 8000))
+@app.post("/scrap")
+def extract_scrap_recipe(url: str = Query(...)):
+    scrap_result = scrap_recipe(url)
+    return scrap_result
 
 if __name__ == "__main__":
+    import os
     import uvicorn
+    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
