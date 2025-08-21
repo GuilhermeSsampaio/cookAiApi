fc73b8aad9b7428a51dc0b6efc03eb68d155a0fd
GuilhermeSsampaio <guilhermessampaio20@gmail.com>
2025-08-20 21:11:40 -0400

    corrogindo denvo

--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 from fastapi import FastAPI, Query
 from fastapi.middleware.cors import CORSMiddleware
 from services.scrap import scrap_recipe
+import os
 
 app = FastAPI()
 
+port = int(os.environ.get("PORT", 8000))
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # Ajuste para restringir em produção
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
 
-@app.get("/", include_in_schema=False)
-@app.head("/", include_in_schema=False)
+@app.get("/")
 def read_root():
     return {"message": "Welcome to the CookAi API"}
 
@@ -23,12 +25,10 @@ def extract_scrap_recipe_get(url: str = Query(...)):
     return scrap_result
 
 @app.post("/scrap")
-def extract_scrap_recipe(url: str = Query(...)):
+def extract_scrap_recipe_post(url: str):
     scrap_result = scrap_recipe(url)
     return scrap_result
 
 if __name__ == "__main__":
-    import os
     import uvicorn
-    port = int(os.environ.get("PORT", 8000))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
